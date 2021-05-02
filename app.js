a17b6c142aeafb7ff58ec65e24840a0c118e661c
Mandy Ho <2018249@student.cct.ie>
2021-05-02 13:37:26 +0000

    final correction

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 //require('dotenv').config()
 
 
-var port = process.env.PORT || 8000;  // Declaring to ports to support heroku
+var port = process.env.PORT || 8000;  // Declare ports to support heroku
 
 
 app.use(bodyParser.json()); // Parsing the data in body to json data
@@ -15,24 +15,24 @@ app.use(bodyParser.json()); // Parsing the data in body to json data
 const partRouter = require('./routes/bakery') // routes for api
 const methodOverride = require('method-override')// package used to update and delete a document via post request.
 
-app.set('view engine', 'ejs')  // pulling up my static webpage
+app.set('view engine', 'ejs')  // pulling up static webpage
 
-app.use(express.urlencoded({ extended: false })) // this is a middlewear function which comes with the express package.
+app.use(express.urlencoded({ extended: false })) // this is a middleware function which comes with the express package.
 // it enable us to use URL-encoded data with the querystring library.
 app.use(methodOverride('_omethod'))// using the method in the app. 
-app.use(express.static('views/bake')) // making my folder public to access my css and javascript for my html page.
+app.use(express.static('views/bake')) // making folder public and easy to access css and javascript for html page.
 
 
 //Render html file homepage
 app.get('/', async (req, res) => {// making a get request
-    const parts = await Part.find();  // this will find all my data from my database collection with the help of model and assign it on to the const parts
+    const parts = await Part.find();  // this will find all data from database collection with the help of model and assign it on to the const parts
 
     res.render('bake/index', { parts: parts })// This will render all the database collection data which was assigned on to the const parts
 })                                           // to my static webpage in views/bake folder and display it to my table on my webpage.
 
 
 // connecting to database
-mongoose.connect(process.env.dbPassword, { useNewUrlParser: true, useUnifiedTopology: true, }) // i am making a connection to the database using 
+mongoose.connect(process.env.dbPassword, { useNewUrlParser: true, useUnifiedTopology: true, }) // make a connection to the database using 
 // my environment variable which is saved in my gitpod. And successfully establishing connection to my database.
 
 const dbc = mongoose.connection
@@ -43,10 +43,10 @@ dbc.on('error', (error) => console.error(error))// checking if there is an error
 
 
 
-app.use('/bakery', partRouter) // 
+app.use('/bakery', partRouter)  
 
 //app.listen(8000, () => console.log('Server Started'))
 
 app.listen(port, function (error) { // Listening to the server on the avaliable port either 5000 or avaliable one. Also show error if their is any.
-    console.log('Server running on port: ' + port);// Server Running Console Message
+    console.log('Server running on port: ' + port); // Server Running Console Message
 });
